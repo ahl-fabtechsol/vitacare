eeda1b4d939b48d9bad065916805c7cd3ab9484e
Ahmed Hassan <131693520+curiousahmed0@users.noreply.github.com>
2025-03-23 18:06:29 +0500

    added nunito

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div className="text-3xl font-bold">Vita care</div>;
+  return (
+    <div className="">
+      <p className="text-3xl font-bold">Vita care</p>
+      <p className="text-2xl font-bold">Vita care</p>
+      <p className="text-xl font-bold">Vita care</p>
+      <p className="text-lg font-bold">Vita care</p>
+      <p className="text-md font-bold">Vita care</p>
+      <p className="text-sm font-bold">Vita care</p>
+      <p className="text-xs font-bold">Vita care</p>
+    </div>
+  );
 }
